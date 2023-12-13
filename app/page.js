bf18d869fcbf2a32b55126544c3dad17f93eafe1
Roman <didoshakr@ukr.net>
2023-12-13 19:05:27 +0200

    1

--- app/page.js
+++ app/page.js
@@ -1,4 +1,3 @@
-// import LayoutWithSidebar from "@/components/rozet/LayoutWithSidebar";
 // import HomePage from "@/components/HomePage";
 import HomePage from "@/components/mag/HomePage";
 import OurProducts from "@/app/(shop)/product/OurProducts";
@@ -8,8 +7,6 @@ import SilverProducts from "@/app/(shop)/product/SilverProducts"
 export default function Home() {
   return (
     <main className="mx-auto min-h-screen w-screen px-1">
-      {/* <LayoutWithSidebar /> */}
-
       <HomePage />
       <section className="flex flex-col space-y-3 px-2 pb-5 pt-5 text-center text-hText">
         {/* <h1 className="tex-lg  font-serif italic">Новий товар</h1> */}
